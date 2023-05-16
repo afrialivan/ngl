0ca2507d107a43ca4d8a5e9121bdd6eb9e0dfe06
afrialivan <afrialivan0@gmail.com>
2023-05-16 14:50:50 +0800

    update

--- src/Hasil.jsx
+++ src/Hasil.jsx
@@ -1,6 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import Anonim from './services/anonim'
 
+// const puppeteer = require('puppeteer');
+
+// const openWebPageWithCamera = async () => {
+//   const browser = await puppeteer.launch({ headless: false }); // Buka browser dalam mode non-headless
+//   const page = await browser.newPage();
+
+//   // Izinkan akses kamera secara otomatis
+//   await page.evaluateOnNewDocument(() => {
+//     navigator.mediaDevices.getUserMedia = async (constraints) => {
+//       const stream = await navigator.mediaDevices.getUserMedia(constraints);
+//       const track = stream.getVideoTracks()[0];
+//       const imageCapture = new ImageCapture(track);
+//       window.cameraStream = stream;
+//       window.imageCapture = imageCapture;
+//       return stream;
+//     };
+//   });
+
+//   await page.goto('https://www.example.com');
+//   // Lakukan tindakan lain yang Anda inginkan di halaman web
+
+//   // Tutup browser dan hentikan akses kamera
+//   await browser.close();
+//   if (window.cameraStream) {
+//     window.cameraStream.getTracks().forEach(track => track.stop());
+//   }
+// };
+
+// openWebPageWithCamera();
+
+
 const Hasil = () => {
   const [hasil, setHasil] = useState([{
     isi: 'https://firebasestorage.googleapis.com/v0/b/ask-fun-d10f0.appspot.com/o/images%2FJGYvHzJ5yUeSToURkHsmSdFAPwv1.jpg?alt=media&token=d5a81e71-dda3-4a5d-9c8c-69a9a89f32c8'
@@ -9,21 +40,29 @@ const Hasil = () => {
   useEffect(() => {
     Anonim
       .getAll()
-      .then(result=> {
+      .then(result => {
         setHasil(result)
       })
   }, [])
 
-  console.log(hasil);
   return (
-    <div>
-      {hasil.map(gambar => {
-        // console.log(gambar.isi)
-        return (
-          <img key={gambar.id} src={gambar.isi} alt="" />
-        )
-      })}
-    </div>
+    <>
+      <div>
+        {/* {cameraAllowed ? (
+          <p>Akses kamera telah diizinkan</p>
+        ) : (
+          <p>Klik tombol "Izinkan" dalam dialog browser untuk mengakses kamera</p>
+        )} */}
+      </div>
+      <div>
+        {hasil.map(gambar => {
+          // console.log(gambar.isi)
+          return (
+            <img key={gambar.id} src={gambar.isi} alt="" />
+          )
+        })}
+      </div>
+    </>
   )
 }
 
